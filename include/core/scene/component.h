07a04db26f2546f21e4087dfb348c8ee19b55fcd
Huseyin <huseyinolgac@gmail.com>
2015-01-28 11:52:36 +0200

    License update to GPLv3;

--- include/core/scene/component.h
+++ include/core/scene/component.h
@@ -1,3 +1,18 @@
+/*
+	This file is part of Vintage Game Engine.
+	Vintage Game Engine is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Vintage Game Engine is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Vintage Game Engine.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef _VGE_COMPONENT_H_
 
 struct vge_component
